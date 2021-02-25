e601cadb9f87d4abc796e03e3568277b57b858a2
Nkenganyi <ntungnkenganyi54@gmail.com>
2021-02-25 19:05:10 +0200

    added the api for search

--- src/main/java/com/nkentech/covid19tracker/controllers/HomeController.java
+++ src/main/java/com/nkentech/covid19tracker/controllers/HomeController.java
@@ -3,11 +3,13 @@ package com.nkentech.covid19tracker.controllers;
 import com.nkentech.covid19tracker.models.LocationStats;
 import com.nkentech.covid19tracker.service.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class HomeController {
@@ -18,6 +20,11 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<LocationStats> home(){
         return coronaVirusDataService.getAllStats();
+    }
+
+    @RequestMapping(value = "/search/{search}", method = RequestMethod.GET)
+    public List<LocationStats>search(@PathVariable(value = "search") String search){
+        return coronaVirusDataService.search(search);
     }
 
 
